85b3022411a9014fdca3f960e658db0594e25f59
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-01-17 20:14:54 +0100

    initial commit for ms-core

---
+++ core.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var passport = require("./passport.js")
+var cors = require('cors');
+
+var App = {
+	Express: {},
+	Server: {},
+	init: function(config) {
+
+		App.Express = express();
+
+		App.Express.use(cors());
+		App.Express.use(bodyParser.urlencoded());
+		App.Express.use(bodyParser.json());
+
+		if(config.enableSecurity){
+			App.Express.use(passport.initialize());
+			App.Express.use(passport.authenticate('jwt', { session: false}));
+		}
+
+		App.Server = App.Express.listen(process.env.PORT || config.port, function() {
+		    console.log("Listening on port %d", App.Server.address().port);
+		});
+	}
+};
+
+module.exports = App;
